4e5128ab7df8de4b7f27b6d236c6a1a5f7f12d9c
FranciszekCzana <franciszek.czana@dazn.com>
2023-02-22 18:29:19 +0100

    Initial Commit

---
+++ BlueMedia Recruitment Task iOSUITests/BlueMedia_Recruitment_Task_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlueMedia_Recruitment_Task_iOSUITestsLaunchTests.swift
+//  BlueMedia Recruitment Task iOSUITests
+//
+//  Created by Franciszek Czana on 22/02/2023.
+//
+
+import XCTest
+
+final class BlueMedia_Recruitment_Task_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
